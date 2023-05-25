b62b55a42058c50ecbfea0a96bbf3c11c942a1f0
esteblock <estebaniglesiasmanriquez@gmail.com>
2023-05-25 13:23:03 -0300

    using soroban-client@0.7.0

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
+import SorobanClient from 'soroban-client'
+
+const myServer = new SorobanClient.Server("https://soroban-rpc.stellar.org")
+console.log("myServer: ", myServer)
 
 function App() {
   return (
